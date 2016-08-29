346a52cee02b696d407f4eadcf3a9cded71242bb
Deyvid Yury <deyvidyury@yahoo.com.br>
2016-08-29 09:58:10 -0300

    Menu toolbar added to home screen

--- app/src/main/java/com/yury/goal/Home.java
+++ app/src/main/java/com/yury/goal/Home.java
@@ -4,6 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutCompat;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.LinearLayout;
@@ -38,5 +41,24 @@ public class Home extends AppCompatActivity implements AdapterView.OnItemClickLi
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
         Project project = Manager.getInstance().getProjects().get(position);
         Toast.makeText(this,project.getName(),Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.home_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.newProject:
+                Toast.makeText(this,"New project",Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
     }
 }
